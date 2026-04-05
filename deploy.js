00263c443419a8c07efbae13c1c3035f141cb820
sameer1ai <raipur4467@gmail.com>
2026-04-05 20:49:06 +0530

    Add package.json configuration for gameshub-bot

--- deploy.js
+++ deploy.js
@@ -1,1 +1,14 @@
-
+{
+  "name": "gameshub-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "start": "node index.js",
+    "deploy": "node deploy.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.14.1",
+    "axios": "^1.6.0"
+  }
+}
